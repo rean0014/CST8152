73e0ae63cc232d5f7f9f60f0a163ea9bf0b9727b
Bianca Reaney Ibarra <rean0014@algonquinlive.com>
2025-08-03 22:18:09 -0400

    final dsl code

---
+++ Quarks/build/CMakeFiles/Compilers.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/code/Compilers.c" "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/build/CMakeFiles/Compilers.dir/Compilers.c.o"
+  "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/code/Main1Coder.c" "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/build/CMakeFiles/Compilers.dir/Main1Coder.c.o"
+  "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/code/Main2Reader.c" "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/build/CMakeFiles/Compilers.dir/Main2Reader.c.o"
+  "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/code/Main3Scanner.c" "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/build/CMakeFiles/Compilers.dir/Main3Scanner.c.o"
+  "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/code/Main4Parser.c" "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/build/CMakeFiles/Compilers.dir/Main4Parser.c.o"
+  "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/code/Main5Writer.c" "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/build/CMakeFiles/Compilers.dir/Main5Writer.c.o"
+  "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/code/Step1Coder.c" "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/build/CMakeFiles/Compilers.dir/Step1Coder.c.o"
+  "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/code/Step2Reader.c" "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/build/CMakeFiles/Compilers.dir/Step2Reader.c.o"
+  "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/code/Step3Scanner.c" "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/build/CMakeFiles/Compilers.dir/Step3Scanner.c.o"
+  "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/code/Step4Parser.c" "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/build/CMakeFiles/Compilers.dir/Step4Parser.c.o"
+  "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/code/Step5Writer.c" "/mnt/c/Users/Bianca/OneDrive - Algonquin College/School/Spring 2025/Compilers/CST8152/Quarks/build/CMakeFiles/Compilers.dir/Step5Writer.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Quarks/build/CMakeFiles/Compilers.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Compilers.dir/Compilers.c.o"
+  "CMakeFiles/Compilers.dir/Main1Coder.c.o"
+  "CMakeFiles/Compilers.dir/Main2Reader.c.o"
+  "CMakeFiles/Compilers.dir/Main3Scanner.c.o"
+  "CMakeFiles/Compilers.dir/Main4Parser.c.o"
+  "CMakeFiles/Compilers.dir/Main5Writer.c.o"
+  "CMakeFiles/Compilers.dir/Step1Coder.c.o"
+  "CMakeFiles/Compilers.dir/Step2Reader.c.o"
+  "CMakeFiles/Compilers.dir/Step3Scanner.c.o"
+  "CMakeFiles/Compilers.dir/Step4Parser.c.o"
+  "CMakeFiles/Compilers.dir/Step5Writer.c.o"
+  "Compilers"
+  "Compilers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Compilers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
